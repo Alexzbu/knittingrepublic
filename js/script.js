ba7864ff12996924e00d34c9a48057764c960b84
Alex <alexzbu@gmail.com>
2025-04-26 09:23:00 +0200

    added product card

--- js/script.js
+++ js/script.js
@@ -13,6 +13,30 @@ function documentActions(e) {
 			document.body.classList.toggle('menu-open');
 		}
 	}
+	if (targetElement.closest('[data-quantity-plus]') || targetElement.closest('[data-quantity-minus]')) {
+		const valueElement = targetElement.closest('[data-quantity]').querySelector('[data-quantity-value]');
+		let value = parseInt(valueElement.value);
+		if (targetElement.hasAttribute('data-quantity-plus')) {
+			value++;
+			if (+valueElement.dataset.quantityMax && +valueElement.dataset.quantityMax < value) {
+				value = valueElement.dataset.quantityMax;
+			}
+		} else {
+			--value;
+			if (+valueElement.dataset.quantityMin) {
+				if (+valueElement.dataset.quantityMin > value) {
+					value = valueElement.dataset.quantityMin;
+				}
+			} else if (value < 1) {
+				value = 1;
+			}
+		}
+		targetElement.closest('[data-quantity]').querySelector('[data-quantity-value]').value = value;
+	}
+	if (targetElement.closest('[data-tabs-button]')) {
+		const currentElement = targetElement.closest('[data-tabs-button]');
+		setTab(currentElement);
+	}
 }
 
 // Sliders
@@ -167,4 +191,31 @@ if (mainProduct) {
 		},
 	});
 
+}
+
+// Tabs
+function setTab(tabElement) {
+	const tabsParent = tabElement.closest('[data-tabs]');
+
+	const tabsButtons = Array.from(tabsParent.querySelectorAll('[data-tabs-button]'));
+	const tabsActiveButton = tabsParent.querySelector('[data-tabs-button].active');
+	tabsActiveButton.classList.remove('active');
+	tabElement.classList.add('active');
+
+	const currentButtonIndex = tabsButtons.indexOf(tabElement);
+	const tabsElements = tabsParent.querySelectorAll('[data-tabs-element]');
+
+	tabsElements.forEach(tabsElement => {
+		tabsElement.hidden = true;
+	});
+
+	tabsElements[currentButtonIndex].hidden = false;
+
+}
+
+function setNewItem(currentElement) {
+	const buttons = Array.from(document.querySelectorAll('[addButton]'));
+	const currentButtonIndex = buttons.indexOf(currentElement);
+	const elements = Array.from(document.querySelectorAll('[addField]'));
+	elements[currentButtonIndex].classList.toggle('hidden')
 }
